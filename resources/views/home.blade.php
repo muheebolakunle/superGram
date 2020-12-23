162898f3d6a751ad40335558ff1417ba4aff1aa3
muheebolakunle <olakunlemuheeb@gmail.com>
2020-12-23 18:37:02 +0100

    create header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('superGram') }}</div>
+    <div class="row">
+        <div class="col-3">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        </div>
+        <div class="col-9">
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
         </div>
     </div>
 </div>
