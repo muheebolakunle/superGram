ed14441a2f4bb2338c71ef309c598cc3246a25c4
muheebolakunle <olakunlemuheeb@gmail.com>
2020-12-24 16:47:29 +0100

    create static ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,32 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3">
-
+        <div class="col-3 p-5">
+            <img src="https://instagram.flos10-1.fna.fbcdn.net/v/t51.2885-19/s150x150/54511297_987747778282765_5659303463331299328_n.jpg?_nc_ht=instagram.flos10-1.fna.fbcdn.net&_nc_ohc=5lpduYyIUIYAX8uAdHj&tp=1&oh=c2d8c93cd47158bc73ed3b2669f4fb46&oe=600ECA40" class="rounded-circle" alt="">
         </div>
-        <div class="col-9">
-
+        <div class="col-9 pt-5">
+            <div>
+                <h1>kunlexy_codes</h1>
+            </div>
+            <div class="d-flex pt-3">
+                <div class="pr-5"><strong>123</strong> posts</div>
+                <div class="pr-5"><strong>12k</strong> followers</div>
+                <div class="pr-5"><strong>204</strong> following</div>
+            </div>
+            <div class="pt-3" style="font-weight: bold;">Olakunle</div>
+            <div>I am a passionate software engineer who enjoys solving problems using technology.</div>
+            <div><a href="#" style="color:black;">kunlexy_codes.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://instagram.flos10-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.126.1030.1030a/s240x240/132108303_387834418947654_9162973108623918750_n.jpg?_nc_ht=instagram.flos10-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=2PxnI3jPKcsAX9p8tME&tp=1&oh=404c9fd768c6a0680f199e420bf31c26&oe=600EE923" class="w-100" />
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.flos10-1.fna.fbcdn.net/v/t51.2885-15/e35/c132.0.564.564a/s320x320/131897168_727687298175032_7267052498875164208_n.jpg?_nc_ht=instagram.flos10-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=2ZyCXRovpgwAX_TQD5p&tp=1&oh=3b9e3ea008120774de835be820ea459c&oe=600D06C3" class="w-100" />
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.flos10-2.fna.fbcdn.net/v/t51.2885-15/e35/c52.0.724.724a/s480x480/131074722_3585003178205266_1692270944517370256_n.jpg?_nc_ht=instagram.flos10-2.fna.fbcdn.net&_nc_cat=110&_nc_ohc=30dnipdOiKkAX_ucJYg&tp=1&oh=cbf60c52d71751be91f227a61e759669&oe=600D5482" class="w-100" />
         </div>
     </div>
 </div>
